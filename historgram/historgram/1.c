13bfef92e9cf1b98384d7ed9024bfa2bf698d51d
hojun <84840627+hojunking@users.noreply.github.com>
2022-04-06 23:07:51 +0900

    update 0406

--- historgram/historgram/1.c
+++ historgram/historgram/1.c
@@ -3,10 +3,18 @@
 #include <math.h>
 #define MAXSIZE 500
 #define COUNTSIZE 10
+#define max(a,b)  (((a) > (b)) ? (a) : (b))
+
+void normal(int count[], int data[]) {
+	printf("\t[히스토그램]\n");
+	for (int i = 0; i < 10; i++)
+		printf("% d | %d\n", i, count[i]);
+}
 
 void graph1(int count[],int data[]) {
 	printf("\n\t[히스토그램 v1.0]\n");
 	for (int i = 0; i < 10; i++) {
+
 		printf("         %d | ", i);
 		for (int j = 0; j < count[i]; j++)
 			printf("*");
@@ -17,10 +25,77 @@ void graph1(int count[],int data[]) {
 void graph2(int count[], int data[]) {
 	printf("\n\t[히스토그램 v2.0]\n");
 	for (int i = 9; 0 <= i; i--) {
-		printf("%d | ", i);
-		//for(int j =)
+
+		if (i == 0) printf("  +");
+		else printf("% d |", i);
+		
+		for (int j = 0;  j <10 ; j++) {
+			if(i==0) printf("---");
+			else{
+				if (i <= count[j])
+					printf("  *");
+				else if (i == count[j] + 1)
+					printf("  %d", count[j]);
+				else
+					printf("   ");
+			}
+		}
+			printf("\n");
 	}
+	printf("      0  1  2  3  4  5  6  7  8  9");
 }
+
+void graph3(int count[], int data[]) {
+	printf("\n\t[히스토그램 v3.0]\n");
+	for (int i = 9; 0 <= i; i--) {
+
+		if (i == 0) printf("   +");
+		else if(i==5) printf("% d |", i);
+		else printf("   |");
+
+		for (int j = 0; j < 10; j++) {
+			if (i == 0) printf("---");
+			else {
+				if (i <= count[j])
+					printf("  %d",i);
+				else
+					printf("   ");
+			}
+		}
+		printf("\n");
+	}
+	printf("      0  1  2  3  4  5  6  7  8  9");
+}
+
+void graph4(int count[], int data[], int size, int high) {
+	double top = (double) high / size;
+	printf("%d\n", high);
+	printf("%.2lf\n", top);
+	
+	int i = top*100/2;
+	printf("\n\t[히스토그램 v4.0]\n");
+	for (; 0 <= i; i--) {
+
+		if (i == 0) printf("  +");
+		else printf("% .2lf | ", (double)i/100*2);
+
+		for (int j = 0; j < 10; j++) {
+			if (i == 0) printf("---");
+			else {
+				double per = (double)count[j] / size;
+				if ((double)i / 100 * 2 + 0.02< per)
+					printf("| |");
+				else if ((double)i / 100 * 2 < per)
+					printf("+-+");
+				else
+					printf("   ");
+			}
+		}
+		printf("\n");
+	}
+	printf("      0  1  2  3  4  5  6  7  8  9");
+}
+
 
 int main(void) {
 
@@ -28,15 +103,16 @@ int main(void) {
 	int count[COUNTSIZE] = {0};
 	int size;
 	
-	while (1) {
+	/*while (1) {
 		printf("데이터의 개수를 입력하시오 (0:종료, 30 <= x <= 500): ");
 		scanf_s("%d", &size);
 		if (size == 0) break;
 		else if (size < 30) {
 			continue;
 		}
-		else if (size < 500) {
+		else if (size < 500) {*/
 			//srand(time(NULL));
+	size = 30;
 			printf("\t[원 랜덤 데이터]\n");
 			for (int i = 0; i < size; i++)
 				data[i] = rand() % 10;
@@ -48,12 +124,16 @@ int main(void) {
 			int  cnt= 0;
 			for (int i = 0; i < size; i++)
 				data[i] == count[data[i]]++;
-			printf("\t[히스토그램]\n");
 
-			for (int i = 0; i < 10; i++)
-				printf("         %d | %d\n", i, count[i]);
-		
-			graph1(count, data);
-		}
-	}
+			//count의 최대값
+			int high = 0;
+			for (int i = 0; i < COUNTSIZE; i++)
+				high = max(count[i], high);
+
+
+			normal(count, data);
+			//graph2(count, data);
+			graph4(count, data, size, high);
+	/*	}
+	}*/
 }
